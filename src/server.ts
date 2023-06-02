bd908533e77cecf352e44891bf3cde278e5e7c8a
Mehraz <mehrazhossain54@gmail.com>
2023-06-03 00:33:43 +0600

    Manage Logs using a library

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { errorlogger, logger } from './shared/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`Database connected`)
+    logger.info(`Database connected`)
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
+      logger.info(`Application listening on port ${config.port}`)
     })
   } catch (error) {
-    console.error(`Failed to connect database`, error)
+    errorlogger.error(`Failed to connect database`, error)
   }
 }
 
